ae0097ff40dcbf2ace44ccd3a694f7f85c92316a
jbh0630 <jayjo0630@gmail.com>
2022-02-17 18:11:23 -0800

    12

---
+++ 12.js
@@ -0,0 +1,11 @@
+var longestCommonPrefix = function(strs) {
+
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let s of strs) {
+            if (s[i] !== strs[0][i]) {
+                return s.slice(0, i);
+            }
+        }
+    }
+    return strs[0];
+};
