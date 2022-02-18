4c45bb18e13dfcfd867ed6ecd682f00bc3b0970a
jbh0630 <jayjo0630@gmail.com>
2022-02-17 18:11:40 -0800

    13

---
+++ 13.js
@@ -0,0 +1,14 @@
+var isValid = function(s) {
+    const stack = [];
+  const complement = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
+
+  for (let char of s) {
+    if (!complement[char]) stack.push(char);
+    else if (stack.pop() !== complement[char]) return false;
+  }
+  return stack.length === 0;
+};
