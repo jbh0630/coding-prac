f8a47e5552d0dfb835cd419fb9f69d77afae75d0
jbh0630 <jayjo0630@gmail.com>
2022-03-03 18:14:19 -0800

    25

---
+++ 25.js
@@ -0,0 +1,15 @@
+var removeElement = function(nums, val) {
+    if (!nums) {
+        return 0;
+    }
+
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+};
